7eb426616f33f1bbd7a68f1029dbe39a68686bc1
pandapid <149129026+pandapid@users.noreply.github.com>
2023-11-15 13:11:25 +0700

    dev

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Owner: {USOW}\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : {USCH}\n○ Support : @impidbot</b>",
+            text = f"<b>○ Owner: {USOW}\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : {USCH}\n○ Support : @devpid</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
