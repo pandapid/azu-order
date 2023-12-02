2f278045a62cde1f95a7010ac6554f71291dbe9b
pandapid <149129026+pandapid@users.noreply.github.com>
2023-12-02 13:27:50 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
 #(©)𝔭𝔞𝔫𝔦𝔪𝔢𝔦𝔡
 
-
-
-
 import os
 import logging
 from logging.handlers import RotatingFileHandler
@@ -10,44 +7,44 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6387715337:AAG-GouYhc0CHNSFMgP0bifHTWNunCY2Weg")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6446016538:AAF3tTj7ERIzuzpDJmpE-qYtnoaRIlf9FFo")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "29684228"))
+APP_ID = int(os.environ.get("APP_ID", "23038554"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "9c201a654e8fc97576ce5c077cf529d2")
+API_HASH = os.environ.get("API_HASH", "696469579b77523faa1b27506e39fe43")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001954090257"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002139139502"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "869513589"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1339534229"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Eln:Chaik2501@cluster0.9ipuplz.mongodb.net/")
-DB_NAME = os.environ.get("DATABASE_NAME", "pusat")
+DB_NAME = os.environ.get("DATABASE_NAME", "anifis")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001900617495"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001823438565"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002080938554"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1002027250940"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {mention} 👋</b>")
+START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {mention} 👋,\nMampir dulu boleh kali Bree \n@animefinish</b>")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "869513589 5531142222").split()):
+    for x in (os.environ.get("ADMINS", "1339534229").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Hello {mention},\n Join dulu bree</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Hello {mention},\n Join dulu ke Channel/ Grup dibawah untuk Ambil File</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -56,12 +53,12 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'False'
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 
-USCH = os.environ.get("USCH", "@pusatofficialbackup")
-USOW = os.environ.get("USOW", "@raden698")
+USCH = os.environ.get("USCH", "@animefinish")
+USOW = os.environ.get("USOW", "@VoStudios1")
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1474271232)
