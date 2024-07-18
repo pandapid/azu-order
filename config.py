197684c3996cd88155d0dcb0ff1ca13ed37946b2
pandapid <149129026+pandapid@users.noreply.github.com>
2024-07-19 00:50:04 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6825910429:AAFXCUqvdV7Kadgo0PCeZp3Wu9vbNbAoZgE")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6017600738:AAFoLXMW51zYMc1QEe9W5BHWidsZGmVrztc")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "28248389"))
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "28248389"))
 API_HASH = os.environ.get("API_HASH", "109d635ff430a728cfc8ecdce9b9cf52")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001990225939"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001832482562"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1889131038"))
@@ -32,13 +32,13 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Eln:Chaik2501@cluster0.9i
 DB_NAME = os.environ.get("DATABASE_NAME", "Aho")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002101999638"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1002106872628"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001863480442"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1002101999638"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {mention} 👋, Enjoy disini.\n\n join @nimeahogb</b>")
+START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {mention} 👋, Enjoy disini.\n\n join channel 18+ @BICEPCLUBZ</b>")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "1889131038 5531142222").split()):
@@ -53,7 +53,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Hello {mention},\n\n Join du
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'False'
